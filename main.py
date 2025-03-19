e5ba1f355d2c38fe48f5a093317e878c7cd479a7
Ellie Alavi <elhamalavi.f@gmail.com>
2025-03-19 14:57:17 -0400

    activity revision

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 def select_dish(foods, selected_food):
-    print(f"Ah, {foods[selected_food]}! An excellent choice!")
+     if selected_food < 0:
+         raise ValueError("Invalid Input!")  # This is Error detection
+    # Select_dish  notices there's something wrong, but doesn't know the best way to handle it. But the menu driver does know how to report that to the user.
+
+     print(f"Ah, {foods[selected_food]}! An excellent choice!")
 
 def your_menu(foods):
     try:
@@ -9,10 +13,15 @@ def your_menu(foods):
             index += 1
     
         selected_choice = int(input("Your order number? "))
+        # We usually seperate error detection from error fix.
+        # if selected_choice < 1:
+        #     raise IndexError("Enter a value between 1 and {len(foods)}")
         select_dish(foods, selected_choice - 1)
-    except IndexError as error:
-        print(f"{error} was entered.")
+
+    except (IndexError, ValueError) as error:
+        print(f"{error} was entered.")  # it didn't show error in case of negative entry
         print("Next time try entering something on the menu!")
+
 
 menu_items = [
     "Yakisoba",
